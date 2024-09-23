4663f5a0df996c32352caab9f8da80dae3d4ff82
gustavo <soaresgustavo07@hotmail.com>
2024-09-23 15:13:38 -0300

    last

---
+++ application/logs/log-2024-09-20.php
@@ -0,0 +1,9 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-09-20 12:33:00 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): No connection could be made because the target machine actively refused it.
+ C:\xampp\htdocs\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2024-09-20 12:33:00 --> Unable to connect to the database
+ERROR - 2024-09-20 12:33:00 --> 404 Page Not Found: Faviconico/index
+ERROR - 2024-09-20 12:33:00 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): No connection could be made because the target machine actively refused it.
+ C:\xampp\htdocs\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2024-09-20 12:33:00 --> Unable to connect to the database
